86969aaf553f5f67d66eef28f3e8767d8225beff
Iscle <albertiscle9@gmail.com>
2023-12-17 22:39:27 +0100

    refactor(gui): convert HeapUsageBar to kt

--- jadx-gui/src/main/java/jadx/gui/ui/HeapUsageBar.kt
+++ jadx-gui/src/main/java/jadx/gui/ui/HeapUsageBar.kt
@@ -1,137 +1,120 @@
-package jadx.gui.ui;
+package jadx.gui.ui
 
-import java.awt.Color;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.util.Objects;
-import java.util.concurrent.TimeUnit;
+import hu.akarnokd.rxjava2.swing.SwingSchedulers
+import io.reactivex.Flowable
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+import jadx.gui.utils.NLS
+import jadx.gui.utils.UiUtils
+import org.slf4j.LoggerFactory
+import java.awt.Color
+import java.awt.event.MouseAdapter
+import java.awt.event.MouseEvent
+import java.util.concurrent.TimeUnit
+import javax.swing.FocusManager
+import javax.swing.JProgressBar
 
-import javax.swing.FocusManager;
-import javax.swing.JProgressBar;
+class HeapUsageBar : JProgressBar() {
+    @Transient private val runtime = Runtime.getRuntime()
+    @Transient private val focusManager = FocusManager.getCurrentManager()
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+    @Transient private var timer: Disposable? = null
 
-import hu.akarnokd.rxjava2.swing.SwingSchedulers;
-import io.reactivex.Flowable;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.schedulers.Schedulers;
+	@Transient private val maxGB: Double
+	@Transient private val limit: Long
+	@Transient private val labelTemplate = NLS.str("heapUsage.text")
 
-import jadx.gui.utils.NLS;
-import jadx.gui.utils.UiUtils;
+	init {
+		isBorderPainted = false
+		isStringPainted = true
 
-public class HeapUsageBar extends JProgressBar {
-	private static final long serialVersionUID = -8739563124249884967L;
+		// Set runtime constants
+		val maxMemory = runtime.maxMemory()
+		maxGB = maxMemory / GB
+		limit = maxMemory - UiUtils.MIN_FREE_MEMORY
 
-	private static final Logger LOG = LoggerFactory.getLogger(HeapUsageBar.class);
+		maximum = (maxMemory / 1024).toInt()
+		foreground = GREEN
 
-	private static final double GB = 1024 * 1024 * 1024d;
-
-	private static final Color GREEN = new Color(0, 180, 0);
-	private static final Color RED = new Color(200, 0, 0);
-
-	private final transient Runtime runtime = Runtime.getRuntime();
-	private final transient FocusManager focusManager = FocusManager.getCurrentManager();
-
-	private final double maxGB;
-	private final long limit;
-	private final String labelTemplate;
-
-	private transient Disposable timer;
-	private transient Color currentColor;
-
-	public HeapUsageBar() {
-		setBorderPainted(false);
-		setStringPainted(true);
-
-		long maxMemory = runtime.maxMemory();
-		maxGB = maxMemory / GB;
-		limit = maxMemory - UiUtils.MIN_FREE_MEMORY;
-		labelTemplate = NLS.str("heapUsage.text");
-
-		setMaximum((int) (maxMemory / 1024));
-		setColor(GREEN);
-
-		addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				Runtime.getRuntime().gc();
-				HeapUsageBar.this.update();
-				if (LOG.isDebugEnabled()) {
-					LOG.debug("Memory used: {}", UiUtils.memoryInfo());
+		addMouseListener(object : MouseAdapter() {
+			override fun mouseClicked(e: MouseEvent) {
+				Runtime.getRuntime().gc()
+				update()
+				if (LOG.isDebugEnabled) {
+					LOG.debug("Memory used: {}", UiUtils.memoryInfo())
 				}
 			}
-		});
+		})
 	}
 
-	@Override
-	public void setVisible(boolean enabled) {
-		super.setVisible(enabled);
-		if (enabled) {
-			startTimer();
-		} else {
-			reset();
-		}
-	}
+    override fun setVisible(enabled: Boolean) {
+        super.setVisible(enabled)
+        if (enabled) {
+            startTimer()
+        } else {
+            reset()
+        }
+    }
 
-	private static class UpdateData {
-		int value;
-		String label;
-		Color color;
-	}
+    private fun startTimer() = synchronized(this) {
+        if (timer != null) return
 
-	private static final UpdateData SKIP_UPDATE = new UpdateData();
-
-	private void startTimer() {
-		if (timer != null) {
-			return;
-		}
-		update();
-		timer = Flowable.interval(2, TimeUnit.SECONDS, Schedulers.newThread())
-				.map(i -> prepareUpdate())
-				.filter(update -> update != SKIP_UPDATE)
-				.distinctUntilChanged((a, b) -> Objects.equals(a.label, b.label)) // pass only if label changed
+		update()
+		timer = Flowable.interval(UPDATE_PERIOD_SECONDS, TimeUnit.SECONDS, Schedulers.newThread())
+				.map { _ -> prepareUpdate() }
+				.filter { it != SKIP_UPDATE }
+				.distinctUntilChanged { a, b -> a.label == b.label }
 				.subscribeOn(SwingSchedulers.edt())
-				.subscribe(this::applyUpdate);
+				.subscribe(this::update)
+    }
+
+	fun reset() = synchronized(this) {
+		if (timer == null) return
+		timer!!.dispose()
+		timer = null
 	}
 
-	public UpdateData prepareUpdate() {
-		if (focusManager.getActiveWindow() == null) {
-			// skip update if app window not active
-			return SKIP_UPDATE;
-		}
-		UpdateData updateData = new UpdateData();
-		long used = runtime.totalMemory() - runtime.freeMemory();
-		updateData.value = (int) (used / 1024);
-		updateData.label = String.format(labelTemplate, used / GB, maxGB);
-		updateData.color = used > limit ? RED : GREEN;
-		return updateData;
-	}
+    private fun prepareUpdate(): UpdateData {
+		// skip update if app window not active
+        if (focusManager.activeWindow == null) return SKIP_UPDATE
 
-	public void applyUpdate(UpdateData update) {
-		setValue(update.value);
-		setString(update.label);
-		setColor(update.color);
-	}
+		val used = runtime.totalMemory() - runtime.freeMemory()
+		return UpdateData(
+				value = (used / 1024).toInt(),
+				label = String.format(labelTemplate, used / GB, maxGB),
+				color = if (used > limit) RED else GREEN
+		)
+    }
 
-	private void setColor(Color color) {
-		if (currentColor != color) {
-			setForeground(color);
-			currentColor = color;
-		}
-	}
+    private fun update(update: UpdateData) {
+		if (update == SKIP_UPDATE) return
+        value = update.value
+        string = update.label
+		foreground = update.color
+    }
 
-	private void update() {
-		UpdateData update = prepareUpdate();
-		if (update != SKIP_UPDATE) {
-			applyUpdate(update);
-		}
-	}
+    private fun update() {
+		update(prepareUpdate())
+    }
 
-	public void reset() {
-		if (timer != null) {
-			timer.dispose();
-			timer = null;
-		}
-	}
+	data class UpdateData(
+			val value: Int = 0,
+			val label: String? = null,
+			val color: Color? = null
+	)
+
+    companion object {
+        private const val serialVersionUID = -8739563124249884967L
+
+        private val LOG = LoggerFactory.getLogger(HeapUsageBar::class.java)
+
+        private const val GB: Double = (1024L * 1024L * 1024L).toDouble()
+
+        private val GREEN = Color(0, 180, 0)
+        private val RED = Color(200, 0, 0)
+
+		private val SKIP_UPDATE = UpdateData()
+
+		private const val UPDATE_PERIOD_SECONDS: Long = 2
+    }
 }
